4f62bc76681c5f1a433dbc3a8810683e83381ebf
yoo-00n <guddbs3891@gmail.com>
2025-06-26 11:15:05 +0900

    영상3

---
+++ humortube/src/App.jsx
@@ -0,0 +1,26 @@
+function App() {
+
+    return (
+    <>
+      <title>LX--</title>
+      <h1 style={{ color: "green" }}>
+        <a href="https://www.lx.or.kr" style={{ color: "green", textDecoration: "none" }}>LXLXLX
+        </a>
+      </h1>
+
+      {/* 노래1 */}
+      <iframe 
+      width="560" 
+      height="315" 
+      src="https://www.youtube.com/embed/WM2b6o-TjNA?si=CC5pkMOHusbieztV&amp;start=8" 
+      title="YouTube video player" 
+      frameborder="0" 
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe><br /><br /><br />
+
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/qdhvdk4FM24?si=V85-XPZiwmHYpFw_&amp;start=8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe><br /><br />
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/uXqjpyhwD_U?si=R_vrGK0gnKpgpUop&amp;start=8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </>
+  )
+}
+
+export default App
---
+++ test.js
@@ -0,0 +1,5 @@
+let a = "아"
+let b = "이"
+let c = a + b
+
+console.log(c)
