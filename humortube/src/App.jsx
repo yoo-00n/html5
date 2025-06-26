a3b496a7a425d863e27dc064b7d2497e350605d5
yoo-00n <guddbs3891@gmail.com>
2025-06-26 13:30:41 +0900

    디자인1

--- humortube/src/App.jsx
+++ humortube/src/App.jsx
@@ -1,26 +1,74 @@
-function App() {
+import { useEffect, useState } from "react";
 
-    return (
-    <>
-      <title>LX--</title>
+function App() {
+  const [authenticated, setAuthenticated] = useState(false);
+
+  useEffect(() => {
+    const answer = prompt("누구냐");
+    if (answer === "조경15") {
+      setAuthenticated(true);
+      document.title = "LX--";
+    } else {
+      alert("꺼지삼");
+    }
+  }, []);
+
+  if (!authenticated) {
+    return <h2 style={{ color: "red" }}>DG3
+      .</h2>;
+  }
+
+  return (
+    <div className="center_div">
+
       <h1 style={{ color: "green" }}>
-        <a href="https://www.lx.or.kr" style={{ color: "green", textDecoration: "none" }}>LXLXLX
+        <a
+          href="https://www.lx.or.kr"
+          style={{ color: "green", textDecoration: "none" }}
+        >
+          LXLXLX
         </a>
       </h1>
 
       {/* 노래1 */}
-      <iframe 
-      width="560" 
-      height="315" 
-      src="https://www.youtube.com/embed/WM2b6o-TjNA?si=CC5pkMOHusbieztV&amp;start=8" 
-      title="YouTube video player" 
-      frameborder="0" 
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe><br /><br /><br />
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/WM2b6o-TjNA?start=8"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      />
 
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/qdhvdk4FM24?si=V85-XPZiwmHYpFw_&amp;start=8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe><br /><br />
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/uXqjpyhwD_U?si=R_vrGK0gnKpgpUop&amp;start=8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-    </>
-  )
+      {/* 노래2 */}
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/qdhvdk4FM24?start=8"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      />
+
+      <h1> 두번 째</h1>
+      
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/uXqjpyhwD_U?start=8"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      />
+
+    </div>
+  );
 }
 
-export default App
+export default App;
